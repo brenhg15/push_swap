f2b6a0f72b9dee8da9dd94d75a72384209451d5f
brenhg15 <brenhg@hotmail.com>
2026-07-06 14:12:02 +0200

    Update disorder_and_index.c

--- disorder_and_index.c
+++ disorder_and_index.c
@@ -22,19 +22,12 @@ static void	coordinate_compression(t_env *env)
 	}
 }
 
-void	calculate_disorder_score(t_env *env)
+static int	count_inversions(t_env *env)
 {
 	int	i;
 	int	j;
 	int	inversions;
-	int	total_pairs;
 
-	coordinate_compression(env);
-	if (env->size < 2)
-	{
-		env->bench.disorder_score = 0.0;
-		return ;
-	}
 	inversions = 0;
 	i = 0;
 	while (i < env->size - 1)
@@ -48,6 +41,23 @@ void	calculate_disorder_score(t_env *env)
 		}
 		i++;
 	}
+	return (inversions);
+}
+
+void	calculate_disorder_score(t_env *env)
+{
+	int	inversions;
+	int	total_pairs;
+
+	coordinate_compression(env);
+	if (env->size < 2)
+	{
+		{
+			env->bench.disorder_score = 0.0;
+			return ;
+		}
+	}
+	inversions = count_inversions(env);
 	total_pairs = (env->size * (env->size - 1)) / 2;
 	env->bench.disorder_score = (double)inversions / total_pairs;
 }
